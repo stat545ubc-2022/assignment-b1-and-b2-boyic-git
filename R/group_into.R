c03d0e118cf2f30937ba83881ef0d518faf6e64f
BoyiChen <21008254+boyic-git@users.noreply.github.com>
2022-11-13 14:04:27 -0800

    create a R package, rm b1 contents

---
+++ R/group_into.R
@@ -0,0 +1,32 @@
+#' Adding a new categorical column into the table 
+#' 
+#' This function will create a new table that includes the original table and a 
+#' new column which is a categorical variable with values depending on the values 
+#' requested by the user.
+#'
+#' @param table original table need to be modified (it makes sense to name the 
+#` variable simple like a single word "table")
+#' @param source_name name of the column to be categorized (the name is intuitive)
+#' @param new_name name of the new column which has categorized values (the name 
+#` is intuitive)
+#' @param categories a vector of categories, it needs to be in ascending order
+#`(because the element in the vector is a category so I take the plural form)
+#'
+#' @return the table with a new categorical column
+#' @export
+#'
+#' @examples 
+#' new_cancer_sample <- group_into(cancer_sample, "area_mean", "area", c("Small", "Medium", "Large"))
+group_into <- function(table, source_name, new_name, categories) {
+  # check if source_name in the table
+  stopifnot(source_name %in% colnames(table))
+  
+  # check length of categories is greater than 0
+  stopifnot(length(categories) > 0)
+  
+  # group the samples depending on the value of source_name 
+  new_table <- table
+  new_table[, new_name] <- cut(table[[source_name]], breaks = length(categories), labels = categories)
+  
+  return(new_table)
+}
--- README.md
+++
@@ -1,8 +0,0 @@
-This repository includes the a `rmd` file which contains the working solution for 
-STAT545 assignment B-1. It also includes a knitted `md` file.  
-
-The assignment B-1 is to create a function in R language and documents the 
-description, examples and tests.  
-
-To use this package, you need to download R and RStudio. It is important to install 
-the `datateachr`, `tidyverse`, `testthat` and `roxygen2` packages in R. 
